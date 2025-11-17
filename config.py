3904f2b8343769f1c6e5e9c7cd185aed0ea4c99a
Peerawat <peerawat2002@gmail.com>
2025-11-17 20:18:26 +0700

    init function

---
+++ config.py
@@ -0,0 +1,16 @@
+CLAIMS_EXTRACTION_PROMPT = "agent-spark-eval-claim_extraction"
+CLAIMS_VERIFICATION_PROMPT = "agent-spark-eval-claim_verification"
+
+EVAL_LEARNER_BG_PROMPT = "agent-spark-eval-learner_background"
+EVAL_KNOWLEDGE_DOMAIN_PROMPT = "agent-spark-eval-knowledge_domain"
+
+EVAL_WHOISTHISFOR_PROMPT = "agent-spark-eval-whoisthisfor"
+EVAL_COMPETENCY_PROMPT = "agent-spark-eval-competency"
+
+EVAL_OVERVIEW_ASSE_PROMPT = "agent-spark-eval-overview_assessment"
+EVAL_ARTIFACT_ASSE_PROMPT = "agent-spark-eval-artifact_assessment"
+EVAL_REFLECTION_ASSE_PROMPT = "agent-spark-eval-reflection_assessment"
+
+EVAL_METHOD_COMP_PROMPT = "agent-spark-eval-mrthod_component"
+EVAL_KEY_METHOD_PROMPT = "agent-spark-eval-key_method"
+EVAL_DESCRIPTION_PROMPT = "agent-spark-eval-description"
